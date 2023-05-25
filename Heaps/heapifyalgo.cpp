54df936215c75bd17b3558e7e3f039ed65f88d9b
shreyaa2506 <shreyaverma2506@gmail.com>
2023-05-26 03:36:45 +0530

    .

---
+++ Heaps/heapifyalgo.cpp
@@ -0,0 +1,24 @@
+void heapify(int arr[],int n, int i, ){
+   int largest = i;
+   int left = 2*i;
+   int right = 2*i+1;
+
+   if(left<=n && arr[left]>arr[largest]){
+    largest = left;
+   }
+   if(right<=n&& arr[right]>arr[largest]){
+    largest = right;
+   }
+   if(largest!=i){
+    swap(arr[i],arr[largest]);
+    heapify(arr,n,largest);
+   }
+
+    
+}
+
+void main(int arr[],int n){
+    for (int i =n/2 ;i>0;i--){
+        heapify(arr,n,i);
+    }
+}
